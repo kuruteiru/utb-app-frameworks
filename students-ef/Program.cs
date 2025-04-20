f2d65fc15eb208b8e25e161ef7c8068c7c8bce4e
kuruteiru <martin.krutil@email.cz>
2025-04-20 22:32:15 +0200

    messing with entity framework

---
+++ students-ef/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Data.Sqlite;
+using students_ef;
+
+SqliteConnectionStringBuilder csb = new() { DataSource = "database.db" };
+string connectionString = csb.ConnectionString;
+
+Student s = new(1, "firstname", "lastname");
+s.Print();
---
+++ students-ef/Student.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace students_ef;
+
+public class Student(int id, string firstName, string lastName, bool active = true)
+{
+	public int ID { get; init; } = id;
+	public string FirstName { get; set; } = firstName;
+	public string LastName { get; set; } = lastName;
+	public bool Active {get; set;} = active;
+
+	public void Print()
+	{
+		Console.WriteLine($"id: {this.ID}");
+		Console.WriteLine($"first name: {this.FirstName}");
+		Console.WriteLine($"last name: {this.LastName}");
+		Console.WriteLine($"active: {this.Active}");
+	}
+}
+
+public class StudentContext : DbContext
+{
+	public DbSet<Student> Students { get; set; }
+}
